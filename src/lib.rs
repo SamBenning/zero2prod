08490fa96733d44e72eff4361353106e1dc4fae9
Sam Benning <bensam1993@gmail.com>
2023-01-25 22:11:31 -0500

    run function now uses Tcp Listener

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
 
+use std::net::TcpListener;
+
 use actix_web::{web, App, HttpResponse, HttpServer, Responder, HttpRequest};
 use actix_web::dev::Server;
 
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
     })
-    .bind("127.0.0.1:8080")?
-    .run();
+        .listen(listener)?
+        .run();
     Ok(server)
 }
 
