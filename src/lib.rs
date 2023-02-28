96a70728377aff67ab3b17ec688d4b4728193ae9
Sam Benning <bensam1993@gmail.com>
2023-02-27 20:33:58 -0500

    Changed project structure and started incorporating postgres.

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,4 @@
-
-use std::net::TcpListener;
-
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, HttpRequest};
-use actix_web::dev::Server;
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String
-}
-
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-        .listen(listener)?
-        .run();
-    Ok(server)
-}
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
